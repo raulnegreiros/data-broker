e5fb17b2b1c2217fb19483cdde5104cd9e2e32db
Matheus Campanha Ferreira <campanha@cpqd.com.br>
2018-08-17 13:20:42 -0300

    Tests and change APIB

---
+++ tests/dredd-hooks/profile-hook.ts
@@ -0,0 +1,31 @@
+"use strict";
+
+import redis = require("redis");
+import config = require("../../src/config");
+import hooks = require("hooks");
+hooks.before("Subject > Subject Profiles > Retrieve subject profile",(transaction: any, done: any) => {
+    let client = redis.createClient(6379, config.cache.redis);
+    client.on('connect', ()=> {
+        console.log("Connected to redis");
+    });
+    client.on('error', function (err) {
+        console.log('Something went wrong ' + err);
+    });
+    client.select(1);
+    let val: any = {
+        "replica_assignment": {
+          "1": [1,2,3],
+          "2": [4,5,6]
+        }
+      };
+    client.set("special-user:devicedata",JSON.stringify(val));
+    val = {
+        "num_partitions": 2,
+        "replication_factor" : 1
+    };
+    client.set("*:devicedata",JSON.stringify(val), () => {
+        done();
+    });
+
+    
+  });
