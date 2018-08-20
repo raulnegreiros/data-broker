5a26edca1905815b2e0b3dba5d560bb12394007a
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2018-08-20 13:53:42 -0300

    feat: update winston to 3.0.0

--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,37 @@
-import * as logger from "winston";
+/*Dojot Logger Library*/
 
-// winston-3.0.0 will have a nice "format" attribute.
-logger.configure({
-  level: "debug",
-  transports: [
-    new logger.transports.Console(),
-  ],
+import winston = require("winston");
+const { combine, timestamp, colorize} = winston.format;
+
+/* Levels of debug */
+const debugLevels = ["debug", "info", "warn", "error"];
+
+function formatParams(info: any) {
+    const { tstamp, level, message, ...args } = info;
+    const ts = tstamp.slice(0, 19).replace("T", " ");
+
+    const filename = Object.keys(args).length ? args.filename : "";
+
+    return `<${ts}> -- |${filename}| -- ${level}: ${message}`;
+}
+
+const logger = winston.createLogger({
+    exitOnError: false,
+    format: combine(
+        winston.format((info: any) => {
+            info.level = info.level.toUpperCase();
+            return info;
+        })(),
+        timestamp({format: "HH:mm:ss DD/MM/YYYY"}),
+        colorize({all: true}),
+        winston.format.printf(formatParams)
+    ),
+    transports: [
+        new winston.transports.Console({
+            handleExceptions: true,
+            level: "debug",
+        }),
+    ],
 });
 
-export { logger };
+export { logger, debugLevels };
